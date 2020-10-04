b900a0324bfe066d78986303c1164cabfcec46e6
Jedidiah May <jedmay83@gmail.com>
2020-10-04 14:50:40 -0500

    First Commit

---
+++ Pages/Aquariums.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace AquaLog.Pages
+{
+    public class Aquariums : PageModel
+    {
+        private readonly ILogger<Aquariums> _logger;
+
+        public Aquariums(ILogger<Aquariums> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
