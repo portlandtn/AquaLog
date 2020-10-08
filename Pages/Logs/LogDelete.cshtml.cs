c71090b6d3fd4f7e68c550a298cea5c57b4a5359
jedmay <jedmay83@gmail.com>
2020-10-07 20:24:57 -0500

    have easier flow with code separated in cards.

---
+++ Pages/Logs/LogDelete.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AquaLog.Core;
+using AquaLog.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace AquaLog.Pages.Logs
+{
+    public class LogDelete : PageModel
+    {
+        private readonly ILogData _logData;
+        public Log Log { get; set; }
+
+        public LogDelete(ILogData logData)
+        {
+            _logData = logData;
+        }
+        public IActionResult OnGet(int logId)
+        {
+            Log = _logData.GetById(logId);
+            if(Log == null)
+            {
+                return RedirectToPage("./Error");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int logId)
+        {
+            var log = _logData.Delete(logId);
+            _logData.Commit();
+
+            if(log == null)
+            {
+                return RedirectToPage("./Error");
+            }
+            TempData["Message"] = $"{log.Id} deleted.";
+            return RedirectToPage("./LogList");
+        }
+    }
+}
+
---
+++ Pages/MeasurementKeys/MeasurementKeyDelete.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using AquaLog.Core;
+using AquaLog.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AquaLog.Pages.MeasurementKeys
+{
+    public class MeasurementKeyDelete : PageModel
+    {
+        private readonly IMeasurementKeyData _measurementKeyData;
+        public MeasurementKey MeasurementKey { get; set; }
+
+        public MeasurementKeyDelete(IMeasurementKeyData measurementKeyData)
+        {
+            _measurementKeyData = measurementKeyData;
+        }
+        public IActionResult OnGet(int measurementKeyId)
+        {
+            MeasurementKey = _measurementKeyData.GetById(measurementKeyId);
+            if(MeasurementKey == null)
+            {
+                return RedirectToPage("./Error");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int measurementKeyId)
+        {
+            var measurementKey = _measurementKeyData.Delete(measurementKeyId);
+            _measurementKeyData.Commit();
+
+            if(measurementKey == null)
+            {
+                return RedirectToPage("./Error");
+            }
+            TempData["Message"] = $"{measurementKey.Name} deleted.";
+            return RedirectToPage("./MeasurementKeyList");
+        }
+    }
+}
+
